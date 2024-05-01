2ece552894cea2b6239b75f329af24ad70d93a4d
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-05-01 23:23:31 +0545

    setup global style providers

---
+++ tasks/app/providers/ContextProvider.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import { GlobalProvider } from "../context/globalProvider";
+interface Props {
+  children: React.ReactNode;
+}
+
+function ContextProvider({ children }: Props) {
+  return <GlobalProvider>{children}</GlobalProvider>;
+}
+
+export default ContextProvider;
