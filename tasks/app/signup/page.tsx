dd910158d128a4aafcd450f1a4e6408fa1a2feb6
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-05-12 22:26:51 +0545

    added hash routing to signin and signup by Clerk

--- tasks/app/signup/page.tsx
+++ tasks/app/signup/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function page() {
   return (
     <div className="flex items-center justify-center h-full">
-      <SignUp />
+      <SignUp routing="hash" />
     </div>
   );
 }
