ddffa71ae65968c8414a022d2d6b98917c36489f
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-05-15 18:43:50 +0545

    pagination fix

--- tasks/app/page.tsx
+++ tasks/app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 import Tasks from "./components/tasks/tasks";
 import { useGlobalState } from "./context/globalProvider";
+import { SignIn, useUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { user } = useUser();
   const { tasks } = useGlobalState();
+  if (!user) {
+    redirect("/signin");
+  }
 
   return <Tasks title="All Tasks" tasks={tasks} />;
 }
