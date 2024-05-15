4250654ff47bf97693f9235a3d55bd45277d7d56
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-05-15 15:46:13 +0545

    created route for updating task status

--- tasks/app/api/tasks/route.ts
+++ tasks/app/api/tasks/route.ts
@@ -66,16 +66,25 @@ export async function GET(req: Request) {
 
 export async function PUT(req: Request) {
   try {
+    const { userId } = auth();
+    const { isCompleted, id } = await req.json();
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized", status: 401 });
+    }
+
+    const task = await prisma.task.update({
+      where: {
+        id,
+      },
+      data: {
+        isCompleted,
+      },
+    });
+
+    return NextResponse.json(task);
   } catch (error) {
     console.log("Error updating task:", error);
     return NextResponse.json({ error: "error updating the task", status: 500 });
-  }
-}
-
-export async function DELETE(req: Request) {
-  try {
-  } catch (error) {
-    console.log("Error deleting task:", error);
-    return NextResponse.json({ error: "error deleting the task", status: 500 });
   }
 }
